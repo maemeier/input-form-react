4f29204f7e738d7da330479655d485b93bb8536a
Mae Kangsadan <maemeier@live.com>
2019-12-27 11:29:31 +0100

    create form component

---
+++ src/form.js
@@ -0,0 +1,58 @@
+import React, { Component } from "react";
+
+class Form extends Component {
+  state = {
+    firstName: "",
+    lastName: "",
+    people: []
+  };
+
+  handleChange = event => {
+    this.setState({
+      [event.target.name]: [event.target.value]
+    });
+  };
+  handlesubmit = event => {
+    event.preventDefault();
+    const firstName = this.state.firstName;
+    const lastName = this.state.lastName;
+    console.log(firstName, lastName);
+    if (firstName.length > 0 && lastName.length > 0) {
+      const person = `${firstName} ${lastName}`;
+      this.setState({
+        people: [...this.state.people, person],
+        firstName: "",
+        lastName: ""
+      });
+    }
+  };
+  render() {
+    return (
+      <section>
+        <article>
+          <form onSubmit={this.handlesubmit}>
+            <input
+              type="text"
+              name="firstName"
+              value={this.state.firstName}
+              onChange={this.handleChange}
+            />
+            <input
+              type="text"
+              name="lastName"
+              value={this.state.lastName}
+              onChange={this.handleChange}
+            />
+            <button type="submit">submit</button>
+          </form>
+        </article>
+        <article>
+          <h1>People on the list</h1>
+          {this.state.people}
+        </article>
+      </section>
+    );
+  }
+}
+
+export default Form;
