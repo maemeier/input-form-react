1d30645491a4e92184930ad5570a1b7b1a5f48c6
Mae Kangsadan <maemeier@live.com>
2019-12-28 16:11:34 +0100

    handleChange

--- src/todoItem.js
+++ src/todoItem.js
@@ -1,8 +1,26 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPen } from "@fortawesome/free-solid-svg-icons";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
 
 class TodoItem extends React.Component {
   render() {
-    return <div>TodoItem Component</div>;
+    const { title, handleDelete, handleEdit } = this.props;
+    return (
+      <div>
+        <li className="list-group-item text-capitalize d-flex justify-content-between my-2">
+          <h6>{title}</h6>
+          <div className="todo-icon">
+            <span className="mx-2 text-secondary" onClick={handleEdit}>
+              <FontAwesomeIcon icon={faPen} />
+            </span>
+            <span className="mx-2 text-danger" onClick={handleDelete}>
+              <FontAwesomeIcon icon={faTrash} />
+            </span>
+          </div>
+        </li>
+      </div>
+    );
   }
 }
 
